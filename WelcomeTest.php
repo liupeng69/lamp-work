4cb9ffdd45b354a28df0da60c049b5b3ab397d15
PL Online <lponline21@gmail.com>
2019-03-18 23:16:29 -0700

    Modified the test to include ! at the end of the message

--- WelcomeTest.php
+++ WelcomeTest.php
@@ -14,7 +14,7 @@
 
         public function testGreet() {
             $result = $this->welcome->greet();
-            $this->assertEquals("Welcome to DevOps on AWS", $result);
+            $this->assertEquals("Welcome to DevOps on AWS!", $result);
         }
 
     }
